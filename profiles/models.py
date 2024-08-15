c22406169e0e033b39ccedbe6b23c1501b68b552
Michael Valdes <michael.j.valdes@gmail.com>
2024-08-15 17:23:58 -0400

    Update profile model and reset migrations

--- profiles/models.py
+++ profiles/models.py
@@ -6,8 +6,9 @@ from django.db import models
 class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.CharField(max_length=18)
-    favorite_commander = models.UUIDField(editable=True)
-    moxfield_id = models.CharField(max_length=50)
-    archidekt_id = models.CharField(max_length=50)
+    favorite_commander = models.UUIDField(
+        editable=True, default=None, blank=True, null=True)
+    moxfield_id = models.CharField(max_length=50, blank=True, null=True)
+    archidekt_id = models.CharField(max_length=50, blank=True, null=True)
     # decks
-    toski_id = models.CharField(max_length=50)
+    toski_id = models.CharField(max_length=50, blank=True, null=True)
