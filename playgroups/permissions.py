733718a6635fe83ba8c6781d358e8db0e79e4f81
Michael Valdes <michael.j.valdes@gmail.com>
2024-08-19 18:57:54 -0400

    Reorder permissions functions

--- playgroups/permissions.py
+++ playgroups/permissions.py
@@ -5,21 +5,6 @@ from rest_framework import permissions
 from playgroups.services import is_playgroup_admin
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.user == request.user
-
-
 class IsPlaygroupAdminOrReadOnly(permissions.BasePermission):
     """
     Custom permission to limit unsafe actions on a playgroup and its 
@@ -27,6 +12,12 @@ class IsPlaygroupAdminOrReadOnly(permissions.BasePermission):
     is obtained from the url route and compared with the permissions of
     the user.
     """
+
+    def has_permission(self, request, view):
+        return self.is_playgroup_admin_or_read_only(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return self.is_playgroup_admin_or_read_only(request, view)
 
     def is_playgroup_admin_or_read_only(self, request, view):
         # Read permissions are allowed to any request,
@@ -38,9 +29,3 @@ class IsPlaygroupAdminOrReadOnly(permissions.BasePermission):
         if request.user.is_authenticated:
             return is_playgroup_admin(request.user, playgroup_id=uuid.UUID(view.kwargs['playgroup_pk']))
         return False
-
-    def has_permission(self, request, view):
-        return self.is_playgroup_admin_or_read_only(request, view)
-
-    def has_object_permission(self, request, view, obj):
-        return self.is_playgroup_admin_or_read_only(request, view)
