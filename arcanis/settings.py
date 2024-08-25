36b86a3950e2668763fc156cbcc2da431731d2f9
Michael Valdes <michael.j.valdes@gmail.com>
2024-08-25 00:45:33 -0400

    Add pagination

--- arcanis/settings.py
+++ arcanis/settings.py
@@ -112,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("knox.auth.TokenAuthentication",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 50,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
